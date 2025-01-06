ae147eae6ce175ba6f684efc9dd3f1e8dd9c91bd
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-01-06 09:04:58 +0300

    init

---
+++ src/test/java/task/tracker/backend/TaskTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package task.tracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
