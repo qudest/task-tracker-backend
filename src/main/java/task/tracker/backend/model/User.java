3ebdc168b129da8d2d80b0f9e53584ead7af17fd
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-01-07 03:35:58 +0300

    feat: do user entity

---
+++ src/main/java/task/tracker/backend/model/User.java
@@ -0,0 +1,46 @@
+package task.tracker.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column(unique = true)
+    String email;
+
+    String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
