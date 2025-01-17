17da422f93f0bb8ea17c47f60151d51752a875d5
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-01-17 04:05:04 +0300

    refactor: remove try-catch

--- src/main/java/task/tracker/backend/service/JwtService.java
+++ src/main/java/task/tracker/backend/service/JwtService.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import task.tracker.backend.model.User;
@@ -15,7 +14,6 @@ import java.util.Date;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Slf4j
 public class JwtService {
 
     @Value("${jwt.secret}")
@@ -38,22 +36,11 @@ public class JwtService {
     }
 
     public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(getSecretKey())
-                    .build()
-                    .parse(token);
-            return true;
-        } catch (ExpiredJwtException e) {
-            log.error("Token expired", e);
-        } catch (UnsupportedJwtException e) {
-            log.error("Unsupported JWT", e);
-        } catch (MalformedJwtException e) {
-            log.error("Malformed JWT", e);
-        } catch (IllegalArgumentException e) {
-            log.error("Invalid token", e);
-        }
-        return false;
+        Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parse(token);
+        return true;
     }
 
     public String getUsernameFromToken(String token) {
