ec0b573b266eb03499a45aeaa7583b19788b2d6e
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-01-17 15:06:13 +0300

    feat: do task entity

---
+++ src/main/java/task/tracker/backend/model/Task.java
@@ -0,0 +1,44 @@
+package task.tracker.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue
+    UUID id;
+
+    String title;
+
+    String description;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @Column(nullable = false)
+    boolean isCompleted = false;
+
+    Instant completedAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (isCompleted) {
+            completedAt = Instant.now();
+        }
+    }
+
+}
