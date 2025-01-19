d1f601a783932f8bb05f0e02f5f8942d09063466
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-01-19 22:54:57 +0300

    feat: add mapper and fix public access

--- src/main/java/task/tracker/backend/service/TaskService.java
+++ src/main/java/task/tracker/backend/service/TaskService.java
@@ -3,9 +3,12 @@ package task.tracker.backend.service;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import task.tracker.backend.dto.TaskDto;
+import task.tracker.backend.exception.TaskNotFoundException;
+import task.tracker.backend.mapper.TaskMapper;
 import task.tracker.backend.model.Task;
 import task.tracker.backend.model.User;
 import task.tracker.backend.repository.TaskRepository;
@@ -19,36 +22,38 @@ import java.util.UUID;
 public class TaskService {
 
     TaskRepository taskRepository;
+    TaskMapper mapper = TaskMapper.INSTANCE;
 
     public List<TaskDto> getTasks() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return taskRepository.findAllByUser(user).stream().map(TaskDto::new).toList();
+        return taskRepository.findAllByUser(user).stream().map(mapper::toDto).toList();
     }
 
     public TaskDto createTask(TaskDto taskDto) {
-        Task task = new Task();
-        task.setTitle(taskDto.title());
-        task.setDescription(taskDto.description());
+        Task task = mapper.toEntity(taskDto);
         task.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         task.setCompleted(false);
-        taskRepository.save(task);
-        return new TaskDto(task);
+        Task saved = taskRepository.save(task);
+        return mapper.toDto(saved);
     }
 
     public TaskDto updateTask(UUID id, TaskDto taskDto) {
-        Task task = new Task();
-        task.setId(id);
-        task.setTitle(taskDto.title());
-        task.setDescription(taskDto.description());
-        task.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        task.setCompleted(taskDto.isCompleted());
-        task.setCompletedAt(taskDto.completedAt());
-        Task saved = taskRepository.save(task);
-        return new TaskDto(saved);
+        Task task = taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
+        if (!task.getUser().equals(SecurityContextHolder.getContext().getAuthentication().getPrincipal())) {
+            throw new AccessDeniedException("You can't update this task");
+        }
+        Task updatingTask = mapper.toEntity(taskDto);
+        updatingTask.setUser(task.getUser());
+        Task saved = taskRepository.save(updatingTask);
+        return mapper.toDto(saved);
     }
 
     public void deleteTask(UUID id) {
-        taskRepository.deleteById(id);
+        Task task = taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
+        if (!task.getUser().equals(SecurityContextHolder.getContext().getAuthentication().getPrincipal())) {
+            throw new AccessDeniedException("You can't delete this task");
+        }
+        taskRepository.delete(task);
     }
 
 }
