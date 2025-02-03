ef00abba14715877cf35f5a2749ab245331c6fca
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-02-04 02:02:33 +0300

    refactor: do jwt service interface

--- src/main/java/task/tracker/backend/service/JwtService.java
+++ src/main/java/task/tracker/backend/service/JwtService.java
@@ -1,55 +1,13 @@
 package task.tracker.backend.service;
 
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import task.tracker.backend.model.User;
 
-import java.security.Key;
-import java.util.Date;
+public interface JwtService {
 
-@Service
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class JwtService {
+    String generateToken(User user);
 
-    @Value("${jwt.secret}")
-    String secretKey;
+    boolean validateToken(String token);
 
-    @Value("${jwt.expiration-time}")
-    long expirationTime;
-
-    public String generateToken(User user) {
-        return Jwts.builder()
-                .setSubject(user.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(getSecretKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    private Key getSecretKey() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
-    }
-
-    public boolean validateToken(String token) {
-        Jwts.parserBuilder()
-                .setSigningKey(getSecretKey())
-                .build()
-                .parse(token);
-        return true;
-    }
-
-    public String getUsernameFromToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSecretKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
+    String getUsernameFromToken(String token);
 
 }
---
+++ src/main/java/task/tracker/backend/service/impl/JwtServiceImpl.java
@@ -0,0 +1,56 @@
+package task.tracker.backend.service.impl;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import task.tracker.backend.model.User;
+import task.tracker.backend.service.JwtService;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class JwtServiceImpl implements JwtService {
+
+    @Value("${jwt.secret}")
+    String secretKey;
+
+    @Value("${jwt.expiration-time}")
+    long expirationTime;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSecretKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSecretKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    public boolean validateToken(String token) {
+        Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parse(token);
+        return true;
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+}
